d763c04078d33297dab80c3d6342bc7619cccca5
Взрослая академия <georgygevorkov@gmail.com>
2026-06-02 21:10:22 +0300

    abdul

--- 02.06.py
+++ 02.06.py
@@ -1,35 +1,59 @@
-"""import math
-num1 = float(input("Ввидите 1-е число: "))
-num2 = float(input("Ввидите 1-е число: "))
-
-print(math.sqrt(num1))
-#print(num1%num2)"""
-
-
-"""num1=int(input("Ввидите число: "))
-
-p1 = num1//100
-p2 = num1//100
-p3 = num1//100
-p4 = num1//10
-
-print(p1+p2+p3+p4)"""
+# import math
+# num1 = float(input("Ввидите 1-е число: "))
+# num2 = float(input("Ввидите 1-е число: "))
+#
+# print(math.sqrt(num1))
+# #print(num1%num2)
 
 
-"""num1=int(input("Ввидите число: "))
+# num1=int(input("Ввидите число: "))
+#
+# p1 = num1//100
+# p2 = num1//100
+# p3 = num1//100
+# p4 = num1//10
+#
+# print(p1+p2+p3+p4)
+#
+#
+# num1=int(input("Ввидите число: "))
+#
+# d1= num1%10
+# d2 = num1//10%10
+# d3 = num1//100%10
+# d4 = num1//1000
+#
+# print(f"{d1}{d2}{d3}{d4}")
 
-d1= num1%10
-d2 = num1//10%10
-d3 = num1//100%10
-d4 = num1//1000
+# num1=int(input("Ввидите число: "))
+#
+# d1= num1%10
+# d2 = num1//10%10
+# d3 = num1//100%10
+# d4 = num1//1000
+#
+# print((d1*1000+d2*100+d3*10+d4)*2)
+num1 = int(input())
 
-print(f"{d1}{d2}{d3}{d4}")"""
+# if num1>0:
+#     print(">0")
+#     print("Конец условия")
+#
+# print("Конец программы")
 
-"""num1=int(input("Ввидите число: "))
+# num1 = int(input())
+#
+# if num1%2==0:
+#     print("четное число")
+# if num1%2!=0:
+#     print("нечетное число")
 
-d1= num1%10
-d2 = num1//10%10
-d3 = num1//100%10
-d4 = num1//1000
 
-print((d1*1000+d2*100+d3*10+d4)*2)"""
+name = input("Ваша имя")
+
+if name=="Ivan":
+    print("yes")
+elif name=="ivan":
+    print("yes")
+else:
+    print("no")
