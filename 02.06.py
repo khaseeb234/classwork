edbfa8d29064932443a68b2e057c4f9d7d85d5cb
Взрослая академия <georgygevorkov@gmail.com>
2026-06-02 19:48:33 +0300

    02.06

---
+++ 02.06.py
@@ -0,0 +1,35 @@
+"""import math
+num1 = float(input("Ввидите 1-е число: "))
+num2 = float(input("Ввидите 1-е число: "))
+
+print(math.sqrt(num1))
+#print(num1%num2)"""
+
+
+"""num1=int(input("Ввидите число: "))
+
+p1 = num1//100
+p2 = num1//100
+p3 = num1//100
+p4 = num1//10
+
+print(p1+p2+p3+p4)"""
+
+
+"""num1=int(input("Ввидите число: "))
+
+d1= num1%10
+d2 = num1//10%10
+d3 = num1//100%10
+d4 = num1//1000
+
+print(f"{d1}{d2}{d3}{d4}")"""
+
+"""num1=int(input("Ввидите число: "))
+
+d1= num1%10
+d2 = num1//10%10
+d3 = num1//100%10
+d4 = num1//1000
+
+print((d1*1000+d2*100+d3*10+d4)*2)"""
