137975a1c5aeeb20abd56d1b7ca2dabafc05b7e0
Взрослая академия <georgygevorkov@gmail.com>
2026-06-08 19:47:36 +0300

    ok

---
+++ 08.06.py
@@ -0,0 +1,21 @@
+# a = int(input())
+# b = int(input())
+# d = int(input())
+#
+# print(a>=b and b <=d)
+
+# a = int(input())
+# b = int(input())
+# if a<b:
+#     a,b = b,a
+# while a>=b:
+#     print(a, end=" ")
+#     a=a-1
+
+a = int(input())
+b = int(input())
+if a<b:
+    a,b = b,a
+while a>=b:
+    print(a, end=" ")
+    a=a-1
