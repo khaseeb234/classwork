417c16276547265f472b318a09aa56103717f176
Взрослая академия <georgygevorkov@gmail.com>
2026-06-11 19:51:02 +0300

    hello

--- 11.05.py
+++ 11.05.py
@@ -4,3 +4,25 @@
 #     print(x)
 #     x += 1
 
+# min1 = int(input())
+# max1 = int(input())
+# min2 = int(input())
+# max2 = int(input())
+# if min1>max1: min1, max1 = max1, min1
+# if min2>max2: min2, max2 = max2, min2
+# if min1>max2:
+#     min = min1
+# else:
+#     min = min2
+# if max1<max2:
+#     max=max1
+# else:
+#     max = max2
+#
+# while min <= max:
+#     print(min, end=" ")
+#     min+=1
+
+
+# a = int(input("введите число: "))
+# print(a%2 and "нечетная" or "Четная")
