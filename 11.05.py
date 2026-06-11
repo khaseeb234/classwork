87419d5e2841f4337c9f431ffd758a55ab82b96f
Взрослая академия <georgygevorkov@gmail.com>
2026-06-11 20:54:03 +0300

    hell

--- 11.05.py
+++ 11.05.py
@@ -3,6 +3,7 @@
 # while x <= a:
 #     print(x)
 #     x += 1
+import a
 
 # min1 = int(input())
 # max1 = int(input())
@@ -26,3 +27,52 @@
 
 # a = int(input("введите число: "))
 # print(a%2 and "нечетная" or "Четная")
+
+# total = 0
+# number = -1
+# while number !=0:
+#     number=int(input("Введите положительное число (или 0 для выхода): "))
+#     if number > 0:
+#         total +=number
+#     elif number < 0:
+#         print("Пожалуйста, введите только положительные числа")
+#
+# print("Сумма положительных числ: ", total)
+
+# a = int(input())
+# b = int(input())
+# if a>b: a,b=b,a
+# for i in range(a,b+1,1):
+#     print(i, end=" ")
+
+# a = int(input())
+# b = int(input())
+# print((a+b)/2)
+
+# пользователь вводить число а и б необходимо ввести каждое трете число в диапозоне от а до б
+
+# a = int(input())
+# b = int(input())
+#
+# for i in range (a,b+1,3):
+#     print(i, end=" ")
+
+# a = int(input())
+# b = int(input())
+# mult=1
+# for i in range (a,b+1,3):
+#     mult *=i
+# print(mult)
+
+
+# num = int(input())
+# maximum = num
+# for i in range(4):
+#     num = int(input())
+#     if maximum<num:
+#         maximum = num
+# print (maximum)
+
+n = int(input())
+for i in range(n):
+    num = int(input())
